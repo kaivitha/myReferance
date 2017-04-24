5380edaa9fae588793c7cf7ae5217cf46e331b68
kavitasri5 <kavitasri5@gmail.com>
2017-04-24 19:22:41 +0530

    my latest commint

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="main.js" />
+var app = angular.module('main', []);
+app.controller("myCtrl", function ($scope) {
+    $scope.message = "here is our new site starts ";
+});
+
+app.controller("welcome", function ($scope) {
+    $scope.message = "welcome to Home Page";
+});
+
+app.controller("aboutCtrl", function ($scope) {
+    $scope.message = "welcome to About Us";
+});
+
+app.controller("solutionsCtrl", function ($scope) {
+    $scope.message = "welcome to Soutions";
+});
+
+app.controller("servicesCtrl", function ($scope) {
+    $scope.message = "welcome to Services";
+});
+
+app.controller("contactCtrl", function ($scope) {
+    $scope.message = "welcome to Contact Us";
+});
---
+++ src/js/route.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular.module("Myapp", ["ngRoute", "main"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/welcome', {
+        templateUrl: "Templates/myhome.html",
+        controller: "welcome"
+    })
+    .when('/about', {
+        templateUrl: "Templates/about.html",
+        controller: "aboutCtrl"
+    })
+	 .when('/services', {
+        templateUrl: "Templates/services.html",
+        controller: "servicesCtrl"
+    })
+    .when('/solutions', {
+        templateUrl: "Templates/solutions.html",
+        controller: "solutionsCtrl"
+    })
+        .when('/contact', {
+            templateUrl: "Templates/contact.html",
+            controller: "contactCtrl"
+        })
+    .otherwise({
+        redirectTo: '/welcome'
+    });
+});
+
